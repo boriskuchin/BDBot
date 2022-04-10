eec55c315105551dbbfdcec7507f70b0f66e2d46
Boris Kuchin <bvkuchin@gmail.com>
2022-04-10 10:50:37 +0300

    Initial commit

---
+++ src/main/java/ru/bvkuchin/bdbot/BDBot/BdBotApplication.java
@@ -0,0 +1,13 @@
+package ru.bvkuchin.bdbot.BDBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bvkuchin/bdbot/BDBot/BdBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bvkuchin.bdbot.BDBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
